6dd1af842538ab74f71033f8da01d0bb70d6b08b
hqx <751156604@qq.com>
2023-12-12 12:17:01 +0800

    Initial commit

---
+++ src/main/java/com/hqx/wzoj/mapper/PostThumbMapper.java
@@ -0,0 +1,17 @@
+package com.hqx.wzoj.mapper;
+
+import com.hqx.wzoj.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ src/main/java/com/hqx/wzoj/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.hqx.wzoj.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hqx.wzoj.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
